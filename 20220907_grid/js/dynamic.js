1772d955fd2553e02b8d0afac27718d70ff7191b
biyakim <w2116@e-mirim.hs.kr>
2022-09-28 11:20:43 +0900

    이전 달 다음 달 완성

--- 20220907_grid/js/dynamic.js
+++ 20220907_grid/js/dynamic.js
@@ -47,3 +47,35 @@ const setCalendar = (year,month) =>{
     firstDateDiv.style.gridColumnStart = firstDay + 1;
 }
 setCalendar(year,month);
+//이전 달 달력 보이자
+const prevMonth = () =>{
+    month--;
+    //month가 0이면, month = 12, year--;
+    if(month == 0){
+        month = 12;
+        year--;
+    }
+    setCalendar(year,month);
+}
+
+//다음 달 달력 보이자
+const nextMonth =()=>{
+    month++;
+    if(month == 13){
+        month = 1;
+        year ++;
+    }
+    setCalendar(year,month);
+}
+const initButton = () => {
+    //HTML -> js
+    // const prev_btn = document.getElementById("prev_btn");
+    // const next_btn = document.getElementById("next_btn");
+
+    //js event 달자
+    // prev_btn.addEventListener("click",prevMonth);
+    // next_btn.addEventListener("click",nextMonth);
+    prev_btn.onclick = prevMonth;
+    next_btn.onclick = nextMonth;
+}
+initButton();
